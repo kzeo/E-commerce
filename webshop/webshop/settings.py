09ca3e6ac75353149d7613998f7d0abe46df2e29
kzo <bartosz.krzeminski24@gmail.com>
2022-08-19 13:03:33 +0200

    Product cards
    done

--- webshop/webshop/settings.py
+++ webshop/webshop/settings.py
@@ -66,6 +66,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'category.context_processors.menu_links',
             ],
         },
     },
@@ -125,7 +126,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
-    'webshop/static',
+    'webshop/static',  
+    
 ]
 
 
