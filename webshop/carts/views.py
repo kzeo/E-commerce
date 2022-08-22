a7fb852cbe2a7c5560d22e225037887eac1cd073
kzo <bartosz.krzeminski24@gmail.com>
2022-08-22 15:16:14 +0200

    Login
    Sign in, registration, dashboard added

--- webshop/carts/views.py
+++ webshop/carts/views.py
@@ -1,4 +1,3 @@
-from http.client import HTTPResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from store.models import Product, Variation
 from .models import Cart, CartItem
