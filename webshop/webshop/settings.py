6c62b9a3bfddd302256d0ce18fcc23d3ad174b52
kzo <bartosz.krzeminski24@gmail.com>
2022-08-25 17:42:55 +0200

    Rating system
    Adding stars and restrictions of it were added

--- webshop/webshop/settings.py
+++ webshop/webshop/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+from django.contrib.messages import constants as messages
 from pathlib import Path
 
 
@@ -129,8 +130,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
-    'webshop/static',  
-    
+    'webshop/static',
+
 ]
 
 
@@ -143,7 +144,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
